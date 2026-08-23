6033a216ac16c014e022fc6efbbf1f2e3527e6c1
sanaa234soso-prog <sanaa234soso@gmail.com>
2026-08-23 05:28:25 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Vireon3/',
-})
+  plugins: [react(), tailwindcss()],
+
+  base: '/vireon3/',
+
+  resolve: {
+    alias: {
+      '@': path.resolve(process.cwd(), '.'),
+    },
+  },
+
+  server: {
+    hmr: process.env.DISABLE_HMR !== 'true',
+    watch: process.env.DISABLE_HMR === 'true' ? null : {},
+  },
+});
